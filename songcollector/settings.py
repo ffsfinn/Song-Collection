f6bed64dd3713aa5571c6be5e9a40cf9ee8390fb
Finn <anthony.torres0926@gmail.com>
2019-12-16 17:14:42 -0700

    Boilerplate Done

--- songcollector/settings.py
+++ songcollector/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'main_app',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -75,10 +76,11 @@ WSGI_APPLICATION = 'songcollector.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'songcollector',
     }
 }
+
 
 
 # Password validation
