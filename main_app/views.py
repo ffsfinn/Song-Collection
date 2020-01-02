32d44b82ea20d2039b6c6e4a5e8796dcefcf00bf
Finn <anthony.torres0926@gmail.com>
2020-01-02 12:28:39 -0600

    Song List Created

--- main_app/views.py
+++ main_app/views.py
@@ -3,9 +3,24 @@ from django.shortcuts import render
 # Add the following import
 from django.http import HttpResponse
 
+class Song:
+    def __init__(self, name, artist, description, year):
+        self.name = name
+        self.artist = artist
+        self.description = description
+        self.year = year
+
+songs = [
+    Song('Midnight Hour', 'Skrillex', 'Recent Drop', 2019),
+    Song('Bangarang', 'Skrillex', 'Older Song', 2011)
+]
+
 # Define the home view
 def home(request):
   return render(request, 'base.html')
 
 def about(request):
     return render(request, 'about.html')
+
+def songs_index(request):
+    return render(request, 'songs/index.html', { 'songs': songs })
